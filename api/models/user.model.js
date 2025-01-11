e01e5e675a8901817901a773da1e4265f287a226
alishkarki <129705823+aliskarkee@users.noreply.github.com>
2025-01-12 03:13:30 +1100

    create and Add OAuth

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -14,7 +14,11 @@ const userSchema = new mongoose.Schema({
     password:{
         type: String,
         required: true,
-    }
+    },
+    avatar:{
+        type: String,
+        default:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLE5nA3jr0oFRiT7X5vAANotebooYLaOXcyw&s"
+    },
 }, {timestamps: true});
 
 const User = mongoose.model('User', userSchema);
